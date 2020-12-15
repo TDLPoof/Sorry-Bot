3bc5a6696b00eb5ff2670f02607f88fd6ac7e38d
penguinshmirtz <penguinshmirtz@gmail.com>
2020-12-15 17:09:55 -0500

    Updated generic player class

--- player.py
+++ player.py
@@ -1,4 +1,16 @@
 class Player(object):
 
     def __init__(self):
-        self.pawns = {1 : ""}
+        self.pawns = {1 : "Start"
+                      2 : "Start"
+                      3 : "Start"
+                      4 : "Start"}
+    
+    def getPawnLocation(self, pawnNum):
+        return self.pawns[pawnNum]
+    
+    def whoToApplyCardTo(self, card):
+        return 1
+
+    def printPlayerInfo(self):
+        print(self.pawns)
