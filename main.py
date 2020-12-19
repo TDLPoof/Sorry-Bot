41b2f5d0348200ba2439ac4f584a0e79c27ceb91
Sasha Tripathi <40964328+TDLPoof@users.noreply.github.com>
2020-12-19 17:44:11 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+Created on Jun 23, 2020
+
+Based on tutorial at https://www.youtube.com/watch?v=6oVZ-VBaC2E
+
+@author: Neil
+'''
+import pygame
+import time
+import config
+from game_state import GameState
+from game import Game
+
+def main(width = 2000, height = 2000, frameRate = 60):
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Tomb Trap")
+    game = Game(screen)
+    game.set_up(0)
+    clock = pygame.time.Clock()
+    #main loop
+    while game.game_state == GameState.RUNNING:
+        #if game.gamePaused:
+        #    screen.blit(pygame.transform.scale(pygame.image.load("sprites/Paus$
+        #    print("did something")
+        clock.tick(frameRate)
+        game.update()
+        pygame.display.flip()
+
+main(config.SCREEN_WIDTH, config.SCREEN_HEIGHT)
